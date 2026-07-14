be943ec4a3b00aac9c577102fce28de5ad43f4b0
htttpmnew-ship-it <htttpmnew@gmail.com>
2026-07-14 10:34:06 +0100

    add

--- src/__main__.py
+++ src/__main__.py
@@ -1,21 +1,43 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from llm_sdk.llm_sdk import Small_LLM_Model
+import torch
 
 class LLM:
     def __init__(self):
-        self.tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
-        self.module = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B")
-    def generate_text(self,prompts):
+        pass
+    def generate_text(self,prompts,llm):
         for prompt in prompts:
-            inputs = self.tokenizer(prompt, return_tensors="pt")
-            outputs = self.module.generate(**inputs, max_new_tokens=50)
-            answer = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+            inputs = llm.encode(prompt)
+            inputs = inputs.tolist()[0]
+            for _ in range(3):
+                # print("heeeeeeeeere ",type(inputs))
+                logits = llm.get_logits_from_input_ids(inputs)
+                logits = torch.tensor(logits)
+                predicted_tensor = torch.argmax(logits)
+                inputs.append(predicted_tensor.item())
+            answer = llm.decode(inputs)
             print(answer)
 
 
 
+
+
+
 S = LLM()
-S.generate_text(["Hi , how are you","there is a story of how made chess board..."])
+llm = Small_LLM_Model()
+S.generate_text(["Hi , how are you ?"],llm)
 
 
 
 
+# def generate(self,prompts):
+#     for prompt in prompts:
+#         for _ in range(0,3):
+#             inputs = self.tokenizer(prompt, return_tensors="pt")
+#             outputs = self.module.generate(**inputs, max_new_tokens=50)
+#             answer = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+#             n = ""
+#             n += prompt + answer
+#             print(n)
+#             prompt = n
+#         print(prompt)
