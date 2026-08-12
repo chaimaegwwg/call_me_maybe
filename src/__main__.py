392a333b115d05c8027d263fda0740799e609287
chaimaegwwg <155653290+chaimaegwwg@users.noreply.github.com>
2026-08-12 19:56:35 +0100

    Update __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -2,19 +2,16 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 from llm_sdk.llm_sdk import Small_LLM_Model
 import torch
 import json
+import sys
 
 
 class LLM:
-    def __init__(self, llm):
+    def __init__(self, llm,vocab):
         with open('/goinfre/cramadan/project/data/input/functions_definition.json', 'r') as file:
             self.functions = json.load(file)
-
+        self.llm = llm
+        self.vocab = vocab
         self.fixed_tokens = {
-            "{": llm.encode("{").tolist()[0],
-            "}": llm.encode("}").tolist()[0],
-            '"': llm.encode('"').tolist()[0],
-            ":": llm.encode(":").tolist()[0],
-            ",": llm.encode(",").tolist()[0],
             "function": llm.encode("function").tolist()[0],
             "arguments": llm.encode("arguments").tolist()[0]
         }
@@ -55,9 +52,9 @@ class LLM:
 
         return None    
     def ft_constrain_one_token(self,parameter,inputs,new_token):
-        logits = llm.get_logits_from_input_ids(inputs)
+        logits = self.llm.get_logits_from_input_ids(inputs)
         logits = torch.tensor(logits)
-        wanted = llm.encode(parameter).tolist()[0][0]
+        wanted = self.llm.encode(parameter).tolist()[0][0]
         
         original_logits = logits.clone()
         logits[:] = float("-inf")
@@ -70,7 +67,14 @@ class LLM:
         new_token.append(predicted_tensor.item())
         inputs.append(predicted_tensor.item())
         return new_token,inputs
-    
+
+    def ft_constrain(self,parameter,inputs,new_token):
+        token = self.vocab[parameter]
+        print("here the debug",token)
+        new_token.append(token)
+        return new_token, inputs
+
+
     def ft_constrain_tokens(self, parameter, inputs, new_token):
         ids = parameter
 
@@ -80,7 +84,7 @@ class LLM:
 
         return new_token, inputs
 
-    def ft_constrain_name_function(self,inputs,new_token):
+    def ft_constrain_name_function(self,inputs,new_token,llm):
         name_of_func = []
         functions = self.all_functions()
         lst_gath_func = []
@@ -106,7 +110,7 @@ class LLM:
             logits[lst_index] = original_logits[lst_index]
             # for n in range(len(logits)):
             #     if n not in lst_index:
-            #         logits[n] = float("-inf")
+            #         logits[n] = float("-inf") 
             predicted_tensor = torch.argmax(logits)
             for fun in lst_gath_func:
                 predicted = predicted_tensor.item()
@@ -123,7 +127,7 @@ class LLM:
             new_token.append(predicted_tensor.item())
             inputs.append(predicted_tensor.item())
         return new_token,inputs,name_of_func
-    def ft_constrain_parameters(self,inputs,new_token,name_of_func):
+    def ft_constrain_parameters(self,inputs,new_token,name_of_func,llm):
         name = llm.decode(name_of_func).strip()
         parameters = self.get_parameters(name)
         if parameters is None:
@@ -173,8 +177,8 @@ class LLM:
             new_token.append(predicted_tensor.item())
             inputs.append(predicted_tensor.item())
         return new_token,inputs,name_of_parameter
-    def ft_numb(self,inputs,new_token):
-        for _ in range(20):
+    def ft_numb(self,inputs,new_token,llm):
+        for _ in range(10):
             logits = llm.get_logits_from_input_ids(inputs)
             logits = torch.tensor(logits)
 
@@ -192,17 +196,17 @@ class LLM:
         # print("the input that will",llm.decode(inputs))
         stop = self.fixed_tokens['"']
         for _ in range(30):
-            logits = llm.get_logits_from_input_ids(inputs)
+            logits = self.llm.get_logits_from_input_ids(inputs)
             logits = torch.tensor(logits)
 
             predicted_tensor = torch.argmax(logits)
             token_id = predicted_tensor.item()
 
-            token_text = llm.decode([token_id])
+            token_text = self.llm.decode([token_id])
 
             # print("---> predict out", repr(token_text))
             if '"' in token_text and "\n" in token_text:
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens['"'],inputs,new_token)
+                new_token,inputs =self.ft_constrain('"',inputs,new_token)
                 # print("the first break")
                 break
             new_token.append(token_id)
@@ -211,9 +215,6 @@ class LLM:
             if '"' in token_text:
                 # print("because it stop here")
                 break
-
-
-
         return new_token, inputs
     def generate_text(self,prompt,llm):
         inputs = llm.encode(prompt)
@@ -222,47 +223,52 @@ class LLM:
         start = 0
         for _ in range(60):
             if start == 0:
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens["{"],inputs,new_token)
+                new_token,inputs =self.ft_constrain("{",inputs,new_token)
                 start +=1
             elif start ==1:
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens['"'],inputs,new_token)
+                new_token,inputs = self.ft_constrain('"',inputs,new_token)
                 new_token,inputs = self.ft_constrain_tokens(self.fixed_tokens["function"],inputs,new_token)
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens['"'],inputs,new_token)
+                new_token,inputs =self.ft_constrain('"',inputs,new_token)
+                print("state 1",llm.decode(new_token))
                 start +=1
             elif start == 2:
-                new_token,inputs = self.ft_constrain_tokens(self.fixed_tokens[":"],inputs,new_token)
+                new_token,inputs = self.ft_constrain(":",inputs,new_token)
                 start +=1
             elif start == 3:
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens['"'],inputs,new_token)
-                new_token,inputs,name_of_func = self.ft_constrain_name_function(inputs,new_token)
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens['"'],inputs,new_token)   
+                print("it reached here the state 3")
+                new_token,inputs =self.ft_constrain('"',inputs,new_token)
+                new_token,inputs,name_of_func = self.ft_constrain_name_function(inputs,new_token,llm)
+                new_token,inputs =self.ft_constrain('"',inputs,new_token)
+                print("state 2",llm.decode(new_token))   
                 start+=1
             elif start == 4:
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens[','],inputs,new_token)
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens['"'],inputs,new_token)
+                new_token,inputs =self.ft_constrain(',' ,inputs,new_token)
+                new_token,inputs =self.ft_constrain('"' ,inputs,new_token)
                 new_token,inputs = self.ft_constrain_tokens(self.fixed_tokens["arguments"],inputs,new_token)
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens['"'],inputs,new_token)
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens[':'],inputs,new_token)
+                new_token,inputs =self.ft_constrain('"',inputs,new_token)
+                new_token,inputs =self.ft_constrain(':',inputs,new_token)
                 start +=1
             elif start == 5:
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens["{"],inputs,new_token)
+                new_token,inputs =self.ft_constrain("{",inputs,new_token)
                 start +=1
             elif start == 6:
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens['"'],inputs,new_token)
-                new_token,inputs,parameter = self.ft_constrain_parameters(inputs,new_token,name_of_func)
+                new_token,inputs =self.ft_constrain('"',inputs,new_token)
+                new_token,inputs,parameter = self.ft_constrain_parameters(inputs,new_token,name_of_func,llm)
 
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens['"'],inputs,new_token)
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens[':'],inputs,new_token)
+                new_token,inputs =self.ft_constrain('"',inputs,new_token)
+                new_token,inputs =self.ft_constrain(':',inputs,new_token)
                 start+=1
             elif start == 7:
+                print("it reached here the state 7")
+                print("state 7",llm.decode(new_token))   
                 parameters_t = llm.decode(parameter).strip()
                 parameter_type = self.parameter_type_func(parameters_t).strip()
                 if parameter_type == "number":
-                    new_token,inputs = self.ft_numb(inputs,new_token)
+                    new_token,inputs = self.ft_numb(inputs,new_token,llm)
                     
                 elif parameter_type == "string":
                     # print("it go here string correctly")
-                    new_token, inputs = self.ft_constrain_tokens(self.fixed_tokens['"'], inputs, new_token)
+                    new_token, inputs = self.ft_constrain('"', inputs, new_token)
                     new_token,inputs = self.ft_string(inputs,new_token)
                     # print(llm.decode(new_token))
                 else:
@@ -270,9 +276,11 @@ class LLM:
                 start+=1
 
             elif start == 8:
+                print("it reached here the state 8")
+                print("state 8",llm.decode(new_token))   
                 logits = torch.tensor(llm.get_logits_from_input_ids(inputs))
-                comma = self.fixed_tokens[","][0]
-                brace = self.fixed_tokens["}"][0]
+                comma = self.vocab[","]
+                brace = self.vocab["}"]
 
                 original_logits = logits.clone()
                 logits[:] = float("-inf")
@@ -288,9 +296,10 @@ class LLM:
                 # print("it reached hereee",llm.decode(new_token))
                 start += 1
             elif start == 9:
+                print("it reached here the state 9")
                 nw = llm.decode(new_token)
                 if token == ",":
-                    new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens[","],inputs,new_token)
+                    new_token,inputs =self.ft_constrain(",",inputs,new_token)
                     start = 6
                 elif token == "}" and nw[-1] == ",":
                     start = 6
@@ -302,8 +311,8 @@ class LLM:
                 # start += 1
             elif start == 10:
                 # print("before the state =10 ",llm.decode(new_token))
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens["}"],inputs,new_token)
-                new_token,inputs =self.ft_constrain_tokens(self.fixed_tokens["}"],inputs,new_token)
+                new_token,inputs =self.ft_constrain("}",inputs,new_token)
+                new_token,inputs =self.ft_constrain("}",inputs,new_token)
                 # print("after the state =10 ",llm.decode(new_token))
                 start +=1
             else:
@@ -316,55 +325,71 @@ class LLM:
         print(repr(answer))
    
 
+def read_vocab(llm):
+    path = llm.get_path_to_vocab_file()
+    try:
+        with open(path,"r") as file:
+            vocab = json.load(file)
+    except FileNotFoundError as e:
+        print(f"Error:",{e})
+        sys.exit(0)
+    return vocab
 
+def maaan_t():
+    llm = Small_LLM_Model()
+    vocab = read_vocab(llm)
+    S = LLM(llm,vocab)
+    # Se = S.gena
+    
+    # laaalm = Small_LLM_Model()
+    # S = LLM(laaalm)
+    with open('/goinfre/cramadan/project/data/input/function_calling_tests.json','r') as file:
+        content = file.read()
+        prompt = json.loads(content)
 
+    with open('/goinfre/cramadan/project/data/input/functions_definition.json','r') as file:
+        functions_text = file.read()
+        # functions = json.loads(content)
+        # functions_text = json.dumps(functions, indent=2)
+    for i in range(11):
+        print("--------------->the promopt",i)
+        user_request = prompt[i]["prompt"]    
+        S.generate_text(f"""You are a function-calling assistant.
 
-llm = Small_LLM_Model()
-S = LLM(llm)
-# laaalm = Small_LLM_Model()
-# S = LLM(laaalm)
-with open('/goinfre/cramadan/project/data/input/function_calling_tests.json','r') as file:
-    content = file.read()
-    prompt = json.loads(content)
+        You are given:
 
-with open('/goinfre/cramadan/project/data/input/functions_definition.json','r') as file:
-    functions_text = file.read()
-    # functions = json.loads(content)
-    # functions_text = json.dumps(functions, indent=2)
+        1. A list of available functions in JSON format.
+        2. A user's request.
 
-for i in range(11):
-    print("--------------->the promopt",i)
-    user_request = prompt[i]["prompt"]    
-    S.generate_text(f"""You are a function-calling assistant.
+        Your task is to determine:
+        - which function should be called,
+        - and what arguments should be passed to it.
 
-    You are given:
+        Available Functions:
 
-    1. A list of available functions in JSON format.
-    2. A user's request.
+        {functions_text}
 
-    Your task is to determine:
-    - which function should be called,
-    - and what arguments should be passed to it.
+        ----------------------------------------
 
-    Available Functions:
+        User Request:
 
-    {functions_text}
+        {user_request}
 
-    ----------------------------------------
+        ----------------------------------------
 
-    User Request:
+        {{
+        "function": "<function_name>",
+        "arguments": {{
+            ...
+        }}
+        }}
 
-    {user_request}
+    Do not explain your reasoning.
+    Do not return Markdown.
+    If no function matches, return null.""",llm)
 
-    ----------------------------------------
-
-    {{
-    "function": "<function_name>",
-    "arguments": {{
-        ...
-    }}
-    }}
-
-Do not explain your reasoning.
-Do not return Markdown.
-If no function matches, return null.""",llm)
+try:
+    maaan_t()
+except KeyboardInterrupt as e:
+    print(f"Error:",{e})
+    sys.exit(0)
